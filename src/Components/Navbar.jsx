04cbf2f8478f0b0d5aaf549ba4dd806a0628eee4
VishalPawar3696 <vishalpawar3696@gmail.com>
2022-06-05 19:10:17 +0530

    Auth-context

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from "@chakra-ui/react";
+import { useContext } from 'react';
+import {AuthContext} from "../Context/AuthCOntext"
+
+ export const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
